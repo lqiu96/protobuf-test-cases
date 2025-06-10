1b4bebe52efa01d89a4e139c37a76a1e0793ff78
Lawrence Qiu <lawrenceqiu@google.com>
2025-06-10 13:31:24 -0400

    chore: Use shall depth for install

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ mkdir tmp
 pushd tmp
 
 # Install protobuf-api and protobuf-sdk locally
-git clone https://github.com/blakeli0/protobuf-poc-split-keep-package.git
+git clone --depth 1 https://github.com/blakeli0/protobuf-poc-split-keep-package.git
 pushd protobuf-poc-split-keep-package
 
 # Install the latest stable branch as (version: 1.0-SNAPSHOT)
@@ -22,24 +22,24 @@ mvn clean install -T 1C
 popd
 
 # Install gax and core with protobuf-java shaded (version: protobuf-shading-within-module)
-git clone https://github.com/googleapis/sdk-platform-java.git --depth=1
+git clone --depth 1 https://github.com/googleapis/sdk-platform-java.git
 pushd sdk-platform-java
 git checkout protobuf-shading-within-module
 mvn clean install -T 1C -DskipTests -Dflatten.skip -Dclirr.skip -Denforcer.skip
 popd
 
 # Install kms, speech, and secretmanager with the shaded modules (version: protobuf-shading-within-module)
-git clone https://github.com/googleapis/google-cloud-java.git --depth=1
+git clone --depth 1 https://github.com/googleapis/google-cloud-java.git
 pushd google-cloud-java
 git checkout protobuf-shading-within-module
-mvn clean install -T 1C -Dflatten.skip -DskipTests -Dcheckstyle.skip -Dclirr.skip -Denforcer.skip -pl 'java-kms/google-cloud-kms,java-speech/google-cloud-speech,java-secretmanager/google-cloud-secretmanager' -am
+mvn clean install -T 1C -Dflatten.skip -DskipTests -Dcheckstyle.skip -Dclirr.skip -Denforcer.skip -pl 'java-kms/google-cloud-kms,java-speech/google-cloud-speech,java-secretmanager/google-cloud-secretmanager,java-notebooks/google-cloud-notebooks' -am
 
 # Install kms with protobuf-sdk shaded with breaking change (version: runtime-only-error)
 git checkout protobuf-shading-runtime-breaking-change-within-module
 mvn clean install -T 1C -Dflatten.skip -DskipTests -Dcheckstyle.skip -Dclirr.skip -Denforcer.skip -pl 'java-kms/google-cloud-kms' -am
 
 # Install kms with protobuf-sdk shaded with breaking change (version: protoc-runtime-breaking-change)
-git checkout protoc-runtime-breaking-change
+git checkout protoc-protobuf-shading-runtime-breaking-change-within-module
 mvn clean install -T 1C -Dflatten.skip -DskipTests -Dcheckstyle.skip -Dclirr.skip -Denforcer.skip -pl 'java-kms/google-cloud-kms' -am
 popd
 
