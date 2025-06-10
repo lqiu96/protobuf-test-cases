dd38c8bf405a9199855bba6a3e7bb7930c19eda8
Lawrence Qiu <lawrenceqiu@google.com>
2025-06-10 15:11:06 -0400

    chore: Remove depth=1 for GH repos as multiple branches are needed

--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+set -e
+
 rm -rf tmp/
 mkdir tmp
 pushd tmp
 
+# Install shared-configs (version: protobuf-configs)
+git clone https://github.com/googleapis/java-shared-config.git
+pushd java-shared-config
+
+git checkout protobuf-configs
+mvn clean install -T 1C
+popd
+
 # Install protobuf-api and protobuf-sdk locally
-git clone --depth 1 https://github.com/blakeli0/protobuf-poc-split-keep-package.git
+git clone https://github.com/blakeli0/protobuf-poc-split-keep-package.git
 pushd protobuf-poc-split-keep-package
 
 # Install the latest stable branch as (version: 1.0-SNAPSHOT)
@@ -22,14 +32,14 @@ mvn clean install -T 1C
 popd
 
 # Install gax and core with protobuf-java shaded (version: protobuf-shading-within-module)
-git clone --depth 1 https://github.com/googleapis/sdk-platform-java.git
+git clone https://github.com/googleapis/sdk-platform-java.git
 pushd sdk-platform-java
 git checkout protobuf-shading-within-module
 mvn clean install -T 1C -DskipTests -Dflatten.skip -Dclirr.skip -Denforcer.skip
 popd
 
-# Install kms, speech, and secretmanager with the shaded modules (version: protobuf-shading-within-module)
-git clone --depth 1 https://github.com/googleapis/google-cloud-java.git
+# Install few java client libraries with the shaded protobuf module (version: protobuf-shading-within-module)
+git clone https://github.com/googleapis/google-cloud-java.git
 pushd google-cloud-java
 git checkout protobuf-shading-within-module
 mvn clean install -T 1C -Dflatten.skip -DskipTests -Dcheckstyle.skip -Dclirr.skip -Denforcer.skip -pl 'java-kms/google-cloud-kms,java-speech/google-cloud-speech,java-secretmanager/google-cloud-secretmanager,java-notebooks/google-cloud-notebooks' -am
