f91c20cd9f72661c6f4f98a3f5ceb4b5fd350963
Lawrence Qiu <lawrenceqiu@google.com>
2025-06-18 17:03:37 -0400

    Remove the gen code test cases

--- protobuf-library/post-split-gen-code/src/main/java/PostSplitGenCode.java
+++
@@ -1,7 +0,0 @@
-import com.example.protobuf.Book;
-
-public class PostSplitGenCode {
-  public static Book createBook(String author, int isbn, String title) {
-    return Book.newBuilder().setAuthor(author).setIsbn(isbn).setTitle(title).build();
-  }
-}
--- protobuf-library/pre-split-gen-code/src/main/java/PreSplitGenCode.java
+++
@@ -1,8 +0,0 @@
-import com.example.protobuf.Book;
-
-public class PreSplitGenCode {
-
-  public static Book createBook(String author, int isbn, String title) {
-    return Book.newBuilder().setAuthor(author).setIsbn(isbn).setTitle(title).build();
-  }
-}
